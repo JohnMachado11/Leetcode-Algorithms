ff3be960efa0de186b2a0ca841deb3ff80e519c2
John Machado <58408039+JohnMachado11@users.noreply.github.com>
2021-03-26 10:48:33 -0500

    Update defangIPaddress.js

--- Easy/Defanging an IP Address/defangIPaddress.js
+++ Easy/Defanging an IP Address/defangIPaddress.js
@@ -6,18 +6,17 @@
  * @param {string} address
  * @return {string}
  */
- var defangIPaddr = function(address) {
+ 
+var defangIPaddr = function(address) {
     return address.replace(/\./g, "[.]");
 };
 
 console.log(defangIPaddr("1.1.1.1"))
 
 // Example 1:
-
 // Input: address = "1.1.1.1"
 // Output: "1[.]1[.]1[.]1"
 
 // Example 2:
-
 // Input: address = "255.100.50.0"
 // Output: "255[.]100[.]50[.]0"
