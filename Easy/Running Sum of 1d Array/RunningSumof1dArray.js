3d63b44a02dd04f9960bc16b2841c19ed3be583d
John Machado <58408039+JohnMachado11@users.noreply.github.com>
2021-03-26 10:47:23 -0500

    Update RunningSumof1dArray.js

--- Easy/Running Sum of 1d Array/RunningSumof1dArray.js
+++ Easy/Running Sum of 1d Array/RunningSumof1dArray.js
@@ -6,6 +6,7 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
 var runningSum = function(nums) {
     let new_array = [];
     let storage = 0;
@@ -26,21 +27,17 @@ var runningSum = function(nums) {
 
 console.log(runningSum([1,2,3,4])); 
 
-
 // Example 1:
-
 // Input: nums = [1,2,3,4]
 // Output: [1,3,6,10]
 // Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
 
 // Example 2:
-
 // Input: nums = [1,1,1,1,1]
 // Output: [1,2,3,4,5]
 // Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
 // Example 3:
 
 // Example 3:
-
 // Input: nums = [3,1,2,10,1]
 // Output: [3,4,6,16,17]
