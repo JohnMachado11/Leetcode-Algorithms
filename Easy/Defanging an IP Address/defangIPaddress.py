d5a1523a0433c09f51b7098607076102e2ed807d
John Machado <58408039+JohnMachado11@users.noreply.github.com>
2021-03-26 10:49:26 -0500

    Update defangIPaddress.py

--- Easy/Defanging an IP Address/defangIPaddress.py
+++ Easy/Defanging an IP Address/defangIPaddress.py
@@ -9,11 +9,9 @@ class Solution:
 print(Solution.defangIPaddr(str, "1.1.1.1"))
 
 # Example 1:
-
 # Input: address = "1.1.1.1"
 # Output: "1[.]1[.]1[.]1"
 
 # Example 2:
-
 # Input: address = "255.100.50.0"
 # Output: "255[.]100[.]50[.]0"
