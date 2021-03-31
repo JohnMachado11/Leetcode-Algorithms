c1be1302f5a172e6928ae1038c28be46ac3f0eab
John Machado <58408039+JohnMachado11@users.noreply.github.com>
2021-03-30 22:59:07 -0500

    Add files via upload

---
+++ Extras/String Reversal/string_reversal.js
@@ -0,0 +1,35 @@
+// Method 1
+
+function reverser(string){
+    let flipped_string = []; 
+    
+    for(let i = string.length; i <= string.length; i--){  
+        if(i < 0){
+            break;
+        }
+        else{
+            flipped_string.push(string[i]);
+        };
+    };
+    return flipped_string.join("");
+};
+
+console.log(reverser("ABCDEFGHIJKLMNOPQRSTUVWXYZ")); // ZYXWVUTSRQPONMLKJIHGFEDCBA
+console.log(reverser("Hey my name is John!")); // !nhoJ si eman ym yeH
+console.log(reverser("I like coding :D")); // D: gnidoc ekil I
+
+// Method 2
+
+function reverser2(string){
+    // turns the string into an array
+    let split_str = string.split("");
+    // reversers the elements of the array
+    let rev_str = split_str.reverse();
+    //combines all the elements of the reversed array into a string
+    let final_str = rev_str.join(""); 
+
+    return(final_str);
+}
+
+console.log(reverser2("Lets see how this works")); // skrow siht woh ees steL
+console.log(reverser2("I like messing with strings, lol.")); // .lol ,sgnirts htiw gnissem ekil I
