e1ddfdabb4367a3b7dfe4eee7a9854a9a684e289
John Machado <58408039+JohnMachado11@users.noreply.github.com>
2021-04-12 00:09:58 -0500

    Add files via upload

---
+++ Easy/Palindrome Number/palindrome_number_faster.js
@@ -0,0 +1,37 @@
+// Leetcode #9
+// Given an integer x, return true if x is palindrome integer.
+// An integer is a palindrome when it reads the same backward as forward. 
+// For example, 121 is palindrome while 123 is not.
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+
+var isPalindrome = function(x) {
+    let reversed_num = x.toString().split("").reverse().join("");
+    return (reversed_num == x ? true : false);
+};
+
+console.log(isPalindrome(121)); // true
+console.log(isPalindrome(-121)); // false
+console.log(isPalindrome(10)); // false
+console.log(isPalindrome(-101)); // false
+
+// Example 1:
+// Input: x = 121
+// Output: true
+
+// Example 2:
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+// Example 3:
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+// Example 4:
+// Input: x = -101
+// Output: false
